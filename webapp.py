9af6d807848f35d5c0c404ccb8202736acc079ef
Your NameE-Perko <edward.p.perko@gmail.com>
2024-10-15 12:18:15 -0700

    add

--- webapp.py
+++ webapp.py
@@ -10,11 +10,16 @@ def render_main():
 def render_response():
     user = request.args['username'] 
     reply1 = "Hello, " + user + "."
+    password = request.args['password'] 
+    password1 = request.args['password1'] 
     gamer = request.args['gamer_scale'] 
-    if gamer == 'no':
-        reply2 = "Unfortunately, you have not met the minimum requirements to join Gamer Club©."
+    if password == password1:
+        if gamer == 'no':
+            reply2 = "Unfortunately, you have not met the minimum requirements to join Gamer Club©."
+        else:
+            reply2 = "You are now a part of Gamer Club©"
     else:
-        reply2 = "You are now a part of Gamer Club©"
+        reply2 = "Your password does not match in both requesting fields. Please return to the previous page to enter your password again."
     return render_template('response.html', response1 = reply1, response2 = reply2)
     
 if __name__=="__main__":
